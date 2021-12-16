b029449e166f2953d04736708564266bebb4aecf
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-16 13:39:28 +0530

    Initial commit

--- api.js
+++ api.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
         $.ajax({
             type: "POST",
             url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
-            data: formData,
-            contentType: "application/json; charset=utf-8",
+            
             data: JSON.stringify(formData),
             dataType: "json",
-            // encode: true,
+            contentType: "application/json; charset=utf-8",
+            encode: true,
             //  success: function(response) {
             //      var myjson = JSON.stringify(response,null,4);
             //      var x = window.open();
