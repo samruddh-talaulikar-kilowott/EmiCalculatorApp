51be5ed53276072e2b66dd982a2956b8271c546d
Sailee-Chitnis-Kilowott <sailee.chitnis@kilowott.com>
2021-12-16 17:34:57 +0530

    initial commit

--- api.js
+++ api.js
@@ -10,8 +10,8 @@
             type: "POST",
             url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
             // contentType: "application/json; charset=utf-8",
-            data : JSON.stringify(formData),
-            // data: formData,
+            // data : JSON.stringify(formData),
+             data: formData,
 
             dataType: "json",
             headers: { "Accept": "application/json", "Content-Type": "application/json" },
