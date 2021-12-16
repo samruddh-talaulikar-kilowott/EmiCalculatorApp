7f28e0333feecb8c0641cbf989f596dc5ac88ca6
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-16 16:13:39 +0530

    Commit

--- api.js
+++ api.js
@@ -9,9 +9,12 @@
         $.ajax({
             type: "POST",
             url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
-            contentType: "application/json; charset=utf-8",
-            data: formData,
+            // contentType: "application/json; charset=utf-8",
+            data : JSON.stringify(formData),
+            // data: formData,
+
             dataType: "json",
+            headers: { "Accept": "application/json", "Content-Type": "application/json" },
           
         });
 
@@ -22,3 +25,30 @@
 
         
     });
+
+
+   
+// document.forms["ApiForm"].addEventListener("submit", e => {
+//          e.preventDefault();
+//          const form = document.forms["ApiForm"];
+//          var api_url = "https://localhost:7092/api/Emi/CalculateDailyEmi";
+//          let response = fetch(api_url, {
+//         method: "POST",
+//         headers: { "Accept": "application/json", "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//             principal: form.elements["principal"].value,
+//             interestRateInPercentage: form.elements["interest"].value,
+//             loanDurationInYearCount: form.elements["duration"].value
+//         })
+//     });
+//     response.then(res =>
+//             res.json()).then(d => {
+//                 console.log(d)
+//                 show(d);
+//             })
+// });
+// function show(d){
+// let emi = d.emiPayment;
+//     document.getElementById("Emi").innerHTML = emi;  
+// }
+     
