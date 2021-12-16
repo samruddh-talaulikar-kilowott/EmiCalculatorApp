74adf84c3adeb2c3fb815c1428bdc816bad6d03b
Sailee-Chitnis-Kilowott <sailee.chitnis@kilowott.com>
2021-12-16 18:37:14 +0530

    initial commit

--- api.js
+++ api.js
@@ -1,29 +1,53 @@
-    $("#button1").click(function() {
-        var formData ={
-            principalAmount: $("#principalAmount").val(),
-            interestRate: $("#interestRate").val(),
-            loanDuration: $("#loanDuration").val(),
-        };
+    // $("#button1").click(function() {
+    //     var formData ={
+    //         principalAmount: $("#principalAmount").val(),
+    //         interestRate: $("#interestRate").val(),
+    //         loanDuration: $("#loanDuration").val(),
+    //     };
 
-        // console.log(formData);
+    //     // console.log(formData);
        
-        $.ajax({
-            type: "POST",
-            url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
-            contentType: "application/json; charset=utf-8",
-            data : JSON.stringify(formData),
-            dataType: "json",
-            // headers: { "Accept": "application/json", "Content-Type": "application/json" },
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
+    //         contentType: "application/json; charset=utf-8",
+    //         data : JSON.stringify(formData),
+    //         dataType: "json",
+    //         // headers: { "Accept": "application/json", "Content-Type": "application/json" },
           
-        });
+    //     });
 
       
 
-        //   event.preventDefault();
+    //     //   event.preventDefault();
 
 
         
-    });
+    // });
 
-
-   
+//AJAX call for MontlyEmi API
+$("#button1").click(function(event) {
+    var formData ={
+        principalAmount: $("#principalAmount").val(),
+        interestRate: $("#interestRate").val(),
+        loanDuration: $("#loanDuration").val(),
+    };
+    $.ajax({
+        type: "POST",
+        url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
+        data: formData,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(formData),
+        dataType: "json",
+        // encode: true,
+        //  success: function(response) {
+        //      var myjson = JSON.stringify(response,null,4);
+        //      var x = window.open();
+        //      x.document.open();
+        //      x.document.write('<html><body><pre>'+ myjson + '</pre></body><html>');
+        //      x.document.close();
+        //     console.log(myjson);
+        //  }
+    })
+     event.preventDefault();
+});   
