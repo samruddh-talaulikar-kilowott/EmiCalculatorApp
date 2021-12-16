b319d71ff59221c3ca3abb1cdd6cb8fdbbc668bb
Sailee-Chitnis-Kilowott <sailee.chitnis@kilowott.com>
2021-12-16 17:45:47 +0530

    initial commit

--- api.js
+++ api.js
@@ -5,19 +5,17 @@
             loanDuration: $("#loanDuration").val(),
         };
 
-        console.log(formData);
+        // console.log(formData);
        
-        // $.ajax({
-        //     type: "POST",
-        //     url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
-        //     // contentType: "application/json; charset=utf-8",
-        //     // data : JSON.stringify(formData),
-        //      data: formData,
-
-        //     dataType: "json",
-        //     headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        $.ajax({
+            type: "POST",
+            url: "https://emicalc-api.herokuapp.com/home/monthlyemi",
+            contentType: "application/json; charset=utf-8",
+            data : JSON.stringify(formData),
+            dataType: "json",
+            // headers: { "Accept": "application/json", "Content-Type": "application/json" },
           
-        // });
+        });
 
       
 
